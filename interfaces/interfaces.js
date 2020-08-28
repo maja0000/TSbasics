331b8f372718bba7f998ddabaa1dc9aa85007fbc
Maja Gach <majaanna94@gmail.com>
2020-08-28 13:22:18 +0200

    interfaces

--- interfaces/interfaces.js
+++ interfaces/interfaces.js
@@ -1,9 +1,22 @@
 var oldCitroen = {
-    year: 1989,
+    year: new Date(),
     broken: true,
-    color: 'red'
+    color: 'red',
+    summary: function () {
+        return "Color is " + this.color;
+    }
 };
-var printVehicle = function (vehicle) {
-    console.log(vehicle.year, vehicle.broken, vehicle.color);
+var drink = {
+    year: new Date(),
+    carbonated: true,
+    color: 'brown',
+    summary: function () {
+        return "My drinks color is " + this.color;
+    }
 };
-printVehicle(oldCitroen);
+var printSummary = function (item) {
+    //   console.log(vehicle.year, vehicle.broken, vehicle.color);
+    console.log(item.summary);
+};
+printSummary(oldCitroen);
+printSummary(drink);
--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -1,16 +1,33 @@
-interface Vehicle {
-  year: Date;
-  broken: boolean;
-  color: string;
+// genetic interface
+
+interface ReportableItem {
+  //   year: Date;
+  //   broken: boolean;
+  //   color: string;
+  summary(): string;
 }
 
 let oldCitroen = {
   year: new Date(),
   broken: true,
   color: 'red',
+  summary(): string {
+    return `Color is ${this.color}`;
+  },
+};
+let drink = {
+  year: new Date(),
+  carbonated: true,
+  color: 'brown',
+  summary(): string {
+    return `My drinks color is ${this.color}`;
+  },
 };
 
-const printVehicle = (vehicle: Vehicle): void => {
-  console.log(vehicle.year, vehicle.broken, vehicle.color);
+// reusable function for both
+const printSummary = (item: ReportableItem): void => {
+  //   console.log(vehicle.year, vehicle.broken, vehicle.color);
+  console.log(item.summary);
 };
-printVehicle(oldCitroen);
+printSummary(oldCitroen);
+printSummary(drink);
