4c74a0a24dd69777ac622652b458334412515bf8
Maja Gach <majaanna94@gmail.com>
2020-08-28 11:48:31 +0200

    folder structure

---
+++ arrays/arrays.js
@@ -0,0 +1,7 @@
+var cars = ['citroen', 'fiat', 'chevy'];
+// let cars: string[] = ['citroen', 'fiat', 'chevy'];
+var carsProduc;
+// array of arrays with strings (type only if initialised but not assigned value)
+carsProduc = [['sss'], ['dddd'], ['fff']];
+var importantDates = [new Date(), 'myBirthday '];
+console.log(importantDates);
---
+++ arrays/arrays.ts
@@ -0,0 +1,9 @@
+let cars = ['citroen', 'fiat', 'chevy'];
+// let cars: string[] = ['citroen', 'fiat', 'chevy'];
+
+let carsProduc: string[][];
+// array of arrays with strings (type only if initialised but not assigned value)
+carsProduc = [['sss'], ['dddd'], ['fff']];
+
+let importantDates: (Date | string)[] = [new Date(), 'myBirthday '];
+// console.log(importantDates);
