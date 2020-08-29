54da6a77a0a695ec72806341bbe92cfbc1fef6b8
Maja Gach <majaanna94@gmail.com>
2020-08-29 12:35:02 +0200

    map object

--- project/src/index.ts
+++ project/src/index.ts
@@ -1,6 +1,14 @@
-import { Company } from './Company';
-import { User } from './User';
+// import { Company } from './Company';
+// import { User } from './User';
 
-const c = new Company();
-const u = new User();
-console.log(c, u);
+// const c = new Company();
+// const u = new User();
+// console.log(c, u);
+import {} from '@types/googlemaps';
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
