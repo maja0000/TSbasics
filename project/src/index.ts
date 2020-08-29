d513dfbf6684cf44676395f7f1799db5a2ef4888
Maja Gach <majaanna94@gmail.com>
2020-08-29 12:57:45 +0200

    custom map

--- project/src/index.ts
+++ project/src/index.ts
@@ -1,14 +1,5 @@
+import { CustomMap } from './CustomMap';
 // import { Company } from './Company';
 // import { User } from './User';
 
-// const c = new Company();
-// const u = new User();
-// console.log(c, u);
-import {} from '@types/googlemaps';
-new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+new CustomMap('map');
