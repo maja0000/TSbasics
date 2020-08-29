b39f4f74d48822610f47b5843e05c3a354babaa9
Maja Gach <majaanna94@gmail.com>
2020-08-29 14:00:27 +0200

    reusable function for both user n company

--- project/src/CustomMap.ts
+++ project/src/CustomMap.ts
@@ -1,3 +1,10 @@
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
   constructor(divId: string) {
@@ -6,6 +13,15 @@ export class CustomMap {
       center: {
         lat: 0,
         lng: 0,
+      },
+    });
+  }
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
       },
     });
   }
--- project/src/index.ts
+++ project/src/index.ts
@@ -1,5 +1,10 @@
 import { CustomMap } from './CustomMap';
-// import { Company } from './Company';
-// import { User } from './User';
+import { Company } from './Company';
+import { User } from './User';
 
-new CustomMap('map');
+const customMap = new CustomMap('map');
+const user = new User();
+const company = new Company();
+
+customMap.addMarker(user);
+customMap.addMarker(company);
