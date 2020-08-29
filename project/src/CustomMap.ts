8850c376f51541cc28b722a4f5bfc5e9539546ff
Maja Gach <majaanna94@gmail.com>
2020-08-29 14:07:34 +0200

    pop up

--- project/src/CustomMap.ts
+++ project/src/CustomMap.ts
@@ -1,3 +1,5 @@
+import { info } from 'console';
+
 interface Mappable {
   location: {
     lat: number;
@@ -17,12 +19,18 @@ export class CustomMap {
     });
   }
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'helo!',
+      });
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
