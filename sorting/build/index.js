6abb8a7f2edc6844560e26646fdbac98972b4495
Maja Gach <majaanna94@gmail.com>
2020-08-30 13:27:11 +0200

    sort algo

--- sorting/build/index.js
+++ sorting/build/index.js
@@ -1,6 +1,28 @@
 "use strict";
-console.log('nodemon doing its thing ?');
-function logMe() {
-    console.log('run me ');
-}
-logMe();
+var Sorter = /** @class */ (function () {
+    //   collection: number[];
+    //   constructor(collection: number[]) {
+    //     this.collection = collection;
+    //   }
+    // SAME AS
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    // swap
+                    var temp = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = temp;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([1, 2, 0, 9, 8, -10]);
+sorter.sort();
+console.log(sorter.collection);
--- sorting/src/index.ts
+++ sorting/src/index.ts
@@ -1,6 +1,24 @@
-console.log('nodemon doing its thing ?');
-
-function logMe() {
-  console.log('run me ');
+class Sorter {
+  //   collection: number[];
+  //   constructor(collection: number[]) {
+  //     this.collection = collection;
+  //   }
+  // SAME AS
+  constructor(public collection: number[]) {}
+  sort(): void {
+    const { length } = this.collection;
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          // swap
+          const temp = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = temp;
+        }
+      }
+    }
+  }
 }
-logMe();
+const sorter = new Sorter([1, 2, 0, 9, 8, -10]);
+sorter.sort();
+console.log(sorter.collection);
