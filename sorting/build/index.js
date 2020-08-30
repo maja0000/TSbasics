51ac583be3e6a67c5cc6a2d3a61e6f14eb8f90a6
Maja Gach <majaanna94@gmail.com>
2020-08-30 14:08:47 +0200

    type guards

--- sorting/build/index.js
+++ sorting/build/index.js
@@ -12,11 +12,16 @@ var Sorter = /** @class */ (function () {
         var length = this.collection.length;
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    // swap
-                    var temp = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = temp;
+                // array type guard
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var temp = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = temp;
+                    }
+                }
+                // string type guard
+                if (typeof this.collection === 'string') {
                 }
             }
         }
--- sorting/src/index.ts
+++ sorting/src/index.ts
@@ -4,16 +4,21 @@ class Sorter {
   //     this.collection = collection;
   //   }
   // SAME AS
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
   sort(): void {
     const { length } = this.collection;
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          // swap
-          const temp = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = temp;
+        // array type guard
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const temp = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = temp;
+          }
+        }
+        // string type guard
+        if (typeof this.collection === 'string') {
         }
       }
     }
